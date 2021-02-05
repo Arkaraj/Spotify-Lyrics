f7bcf2e29c1264b2b3383925100b5f335ea11d52
Arkaraj <arkaraj2017@gmail.com>
2021-02-05 15:57:44 +0530

    Added colour and some UI changes

--- Spotify/app.js
+++ Spotify/app.js
@@ -4,7 +4,10 @@ const nameOfSong = document.querySelector('#name')
 const artist = document.querySelector('#singer')
 const img = document.querySelector('#img')
 const lyrics = document.querySelector('#lyrics')
-const Lyrics_URL = "https://colorflyv1.herokuapp.com/v1//lyrics/"
+const Lyrics_URL = "https://colorflyv1.herokuapp.com/v1/lyrics/"
+const Colour_URL = "https://colorflyv1.herokuapp.com/v1/color/"
+const buttons = document.querySelectorAll('button')
+const player = document.querySelector('#ctrl')
 
 // async function populateSong() {
 //     const header = {
@@ -27,6 +30,23 @@ const Lyrics_URL = "https://colorflyv1.herokuapp.com/v1//lyrics/"
 
 // }
 
+async function getButtonColor(url) {
+    const header = {
+        headers: {
+            "content-type": "application/json"
+        }
+    }
+    const site = await fetch(url, header)
+    const response = await site.json()
+    // console.log(response)
+
+    buttons.forEach(btn => {
+        btn.style.backgroundColor = response.color_0;
+
+    })
+
+}
+
 async function Lyrics(opts) {
     const header = {
         headers: {
@@ -39,17 +59,17 @@ async function Lyrics(opts) {
 
     const data = await url.json()
     // fix for undefined
-    if (data["lyrics"] === undefined) {
+    if (data.lyrics === undefined) {
         lyrics.innerHTML = 'No Lyrics found for this song 🙁'
     } else
-        lyrics.innerHTML = data["lyrics"]
+        lyrics.innerHTML = data.lyrics
 }
 
 function populateSong() {
     spotify.getTrack((err, track) => {
         if (err) console.log(err)
-        let name = track['name']
-        let singer = track['artist']
+        let name = track.name
+        let singer = track.artist
 
         if (nameOfSong.value !== name) {
 
@@ -60,17 +80,21 @@ function populateSong() {
             nameOfSong.value = name
             // Ad
             if (singer === '') {
-                let imgage = track['artwork_url']
+                let imgage = track.artwork_url
                 nameOfSong.innerHTML = name
-                artist.innerHTML = 'Spotify Advertisment ☹️'
+                artist.innerHTML = 'Spotify Advertisment 😟'
                 img.src = imgage
                 lyrics.innerHTML = 'Advertisment...<br>Stay tuned!'
+                buttons.forEach(btn => {
+                    btn.style.backgroundColor = '#00C080';
+
+                })
             } else {
-                let imgage = track['artwork_url']
+                let imgage = track.artwork_url
                 nameOfSong.innerHTML = name
                 artist.innerHTML = singer
                 img.src = imgage
-
+                getButtonColor(Colour_URL + `${track.artwork_url}`)
                 Lyrics(parameters);
             }
 
